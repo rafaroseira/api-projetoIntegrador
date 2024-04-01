276883328d0427010576d858b0f17eafc216ce7a
Rafael Roseira Machado <rafaroseira27@outlook.com>
2024-04-01 10:12:52 -0300

    Atualizado o cadastro de cliente

--- src/main/java/com/example/projeto/api/controller/ClienteController.java
+++ src/main/java/com/example/projeto/api/controller/ClienteController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.projeto.api.model.Cliente;
 import com.example.projeto.api.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,8 +17,9 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @PostMapping("/cadastro/cliente")
-    public Cliente cadastrar(@RequestBody Cliente cliente) {
-        return clienteService.cadastrar(cliente);
+    public ResponseEntity<String> cadastrar(@RequestBody Cliente cliente) {
+        clienteService.cadastrar(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Cliente cadastrado com sucesso!");
     }
     
 }
--- src/main/java/com/example/projeto/api/repository/PetShopRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.projeto.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.projeto.api.model.PetShop;
-
-public interface PetShopRepository extends CrudRepository<PetShop,Integer> {
-
-}
